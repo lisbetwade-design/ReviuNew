1955d6c85ff05ea84376d96d8e9c1ac37cb00bd9
lisbetwade-design <lisbet.wade@gmail.com>
2026-01-28 08:00:54 +0100

    Updated index.ts

--- supabase/functions/figma-oauth-start/index.ts
+++ supabase/functions/figma-oauth-start/index.ts
@@ -112,7 +112,7 @@ Deno.serve(async (req: Request) => {
       );
     }
 
-    const scopes = "files:read";
+    const scopes = "file_content:read";
     const authUrl = new URL("https://www.figma.com/oauth");
     authUrl.searchParams.set("client_id", clientId);
     authUrl.searchParams.set("redirect_uri", redirectUri);
